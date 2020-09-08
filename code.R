695caa0339f00ccda7e31c26f323ff31fb116362
r-Saikat <roy.saikatroy99@gmail.com>
2020-09-08 14:23:11 +0530

    Formule added and pyramid percentage corrected
    The formulae of the population indicators were added. Some explanatory comments were added in the pdf file.
    The percentage values of the population pyramid were wrong. They were corrected and the pyramids were updated with the corrected population values. The denominator of the population ratio in each sex-age group should be the total population of the same year and not the sex-specific total population of that year.

--- code.R
+++ code.R
@@ -8,8 +8,8 @@ dat1985 <- read.csv("1985.csv")
 #class(dat1985)
 colnames(dat1985)<-c("Age_groups", "Males","Females")
 attach(dat1985)
-m <- Males/sum(Males) * 100
-f <- Females/sum(Females) *100
+m <- Males/(sum(Males)+sum(Females)) * 100
+f <- Females/(sum(Females)+sum(Females)) *100
 
 z <- data.frame(m,f,Age_groups)
 z
@@ -24,8 +24,8 @@ dat2015 <- read.csv("2015.csv")
 dat2015
 
 attach(dat2015)
-m1 <- dat2015$Male/sum(dat2015$Male) * 100
-f1 <- dat2015$Female/sum(dat2015$Female) * 100
+m1 <- dat2015$Male/(sum(dat2015$Male)+sum(dat2015$Female)) * 100
+f1 <- dat2015$Female/(sum(dat2015$Female)+sum(dat2015$Female)) * 100
 
 z1 <- data.frame(m1,f1,Age_groups)
 z1
